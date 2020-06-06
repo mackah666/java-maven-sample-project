83abe5f49c1c55438bb495273816016887dcbc74
mackah666 <mackah666@yahoo.co.uk>
2020-06-06 03:16:42 +0100

    Added some example class and attempting to generate javadocs along the way

---
+++ src/main/java/com/mycompany/app/Address.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+
+public class Address implements Cloneable
+{
+    public String street;
+    public String city;
+    public String state;
+    public String zipCode;
+}
